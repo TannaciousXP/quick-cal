c2c7fcee339998ad4d7f7f5aa73a93d1e978e397
Peter Tan <pxhtan@gmail.com>
2017-10-31 18:41:14 -0700

    render buttons

--- lookups.md
+++ lookups.md
@@ -4,9 +4,16 @@
 #Express
 - look up if it's app.connect or app.listen
 
+
+#Client
+- raw js to call server fetch()
+
 ``
 Intersting enough, for the fetch request, I only needed the url, and method to send over the request. To send back the response, I use res.setHeader(key, value) then res.json(response). In index.html I still needed to return res.json(), so it was interesting using two res.json;
 ``
 
-#Client
-- raw js to call server
+#JS DOM
+- add event click
+
+#HTML
+- flat colors
--- scripts.js
+++ scripts.js
@@ -1,3 +1,48 @@
+let d = document;
+
 function addButtons () {
-  
-};
+  let cal = d.getElementById('cal');
+  let display = createEleWithAttr('div', {'id': 'display'});
+  cal.appendChild(display);
+
+  let buttonList = [7, 8, 9, '*', 4, 5, 6, '+', 0, '.', '='];
+  let counter = 0;
+  let row = createEleWithAttr('div', {'class': 'row'});
+  let button;
+
+  for (let i in buttonList) {
+    button = createEleWithAttr('div', {'class': 'button', 'onclick': `pressButton()`});
+    if (counter >= 4) {
+      cal.appendChild(row);
+      row = createEleWithAttr('div', {'class': 'row'});
+      counter = 0;
+    } 
+    if (isNaN(buttonList[i])) {
+      button.setAttribute('class', 'button operator');
+    }
+    if (buttonList[i] === 0) {
+      button.style.cssText = 'width: 60px';
+    }
+    button.innerHTML = buttonList[i];
+    row.appendChild(button);
+    counter++;
+  }
+  cal.appendChild(row);
+}
+
+function createEleWithAttr(ele, attr) {
+  let element = d.createElement(ele);
+  for (let k in attr) {
+    element.setAttribute(k, attr[k]);
+  }
+  return element;
+}
+
+function pressButton (e) {
+  let display = d.getElementById('display').innerHTML;
+  console.log(e);
+}
+
+function eval () {
+
+}
