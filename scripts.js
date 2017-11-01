3754b7ef9d3d13aba15e3ccdd1253d3ad07de19b
Peter Tan <pxhtan@gmail.com>
2017-10-31 19:28:42 -0700

    finish cal

--- scripts.js
+++ scripts.js
@@ -3,15 +3,17 @@ let d = document;
 function addButtons () {
   let cal = d.getElementById('cal');
   let display = createEleWithAttr('div', {'id': 'display'});
+  display.innerHTML = '';
   cal.appendChild(display);
 
-  let buttonList = [7, 8, 9, '*', 4, 5, 6, '+', 0, '.', '='];
+  let buttonList = [7, 8, 9, '*', 4, 5, 6, '+', 3, 2, 1, '/', 0, '.', 'C', '='];
   let counter = 0;
   let row = createEleWithAttr('div', {'class': 'row'});
   let button;
 
   for (let i in buttonList) {
-    button = createEleWithAttr('div', {'class': 'button', 'onclick': `pressButton()`});
+    button = createEleWithAttr('div', {'class': 'button'});
+    button.addEventListener('click', pressButton);
     if (counter >= 4) {
       cal.appendChild(row);
       row = createEleWithAttr('div', {'class': 'row'});
@@ -20,9 +22,7 @@ function addButtons () {
     if (isNaN(buttonList[i])) {
       button.setAttribute('class', 'button operator');
     }
-    if (buttonList[i] === 0) {
-      button.style.cssText = 'width: 60px';
-    }
+
     button.innerHTML = buttonList[i];
     row.appendChild(button);
     counter++;
@@ -39,10 +39,27 @@ function createEleWithAttr(ele, attr) {
 }
 
 function pressButton (e) {
-  let display = d.getElementById('display').innerHTML;
-  console.log(e);
+  let display = d.getElementById('display');
+  let press = e.target.innerHTML;
+  let curr = display.innerHTML;
+  if (isNaN(curr[curr.length - 1]) && (press ==='*' || press === '+' || press === '/')) {
+    // Do nothing
+  } else {
+    if(press === 'C') {
+      display.innerHTML = '';
+  } else if (press === '=') {
+    let request = new Request(`http://localhost:3000/cal/${curr}`, {
+      'method': 'GET',
+    })
+    fetch(request).then(res => {
+      return res.json();
+    }).then(results => display.innerHTML = results).catch(err => console.log('Request failed', err));
+  } else {
+    curr += press;
+    display.innerHTML = curr;
+  }  
+  }
 }
 
-function eval () {
 
-}
+
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/cal/:eval', (req, res) => {
   console.log(req.params);
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json('NEW INFO sup!');
+  res.json(eval(req.params.eval));
 });
 
 app.listen(3000, () => {
